48a47c3d83ca34496a7a7d54acdfd80b97a7ff83
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-08-05 11:11:03 -0400

    Update ParseRAMWithVol.sh

--- ParseRAMWithVol.sh
+++ ParseRAMWithVol.sh
@@ -15,82 +15,82 @@ else
     foldername="DUMP_$foldername"
     mkdir $foldername
     echo "- Created folder $foldername, parsed files will be placed in that folder"
-	echo
- 	echo "Creating strings file with 8 char minimum as strings.txt ..."
+    echo
+    echo "Creating strings file with 8 char minimum as strings.txt ..."
     strings -a -t d -n 8 $1 > $foldername\strings.txt
     echo "Creating command line dump as cdmline.txt ... 1/24"
     vol -f $1 -v  windows.cmdline > $foldername\cmdline.txt
     echo
     echo "Creating DLL list into dlllist.txt ... 2/24"
-	vol -f $1 -v   windows.dlllist > $foldername\dlllist.txt
-	echo
-	echo "Creating driver list into driverlist.txt ... 3/24"
-	vol -f $1 -v  windows.driverscan > $foldername\driverlist.txt
+    vol -f $1 -v   windows.dlllist > $foldername\dlllist.txt
     echo
-	echo "Creating list of environment variables as envlist.txt ... 4/24"
-	vol -f $1 -v  windows.envars > $foldername\envlist.txt
-	echo
-	echo "Creating list of files in memory as filelist.txt .... 5/24"
-	vol -f $1 -v  windows.filescan > $foldername\filelist.txt
-	echo
-	echo "Creating list of owner SIDs for each process as ownersidslist.txt ... 6/24"
-	vol -f $1 -v  windows.getsids > $foldername\ownersidslist.txt
-	echo
-	echo "Creating list of open file handles into handleslist.txt ... 7/24"
-	vol -f $1 -v  windows.handles > $foldername\handleslist.txt
-	echo
-	echo "Creating list of memory ranges with potentially injected code as malmemcode.txt ... 8/24"
-	vol -f $1 -v  windows.malfind > $foldername\malmemcode.txt
-	echo 
-	echo "Creating list of potentially injected by rootkit drivers as rootkitsdrivers.txt ... 9/24"
-	vol -f $1 -v  windows.malware.drivermodule > $foldername\rootkitsdrivers.txt
-	echo
-	echo "Creating list of potentially malware hollowed proccess as malhollow.txt ... 10/24"
-	vol -f $1 -v  windows.malware.hollowprocesses > $foldername\malhollow.txt
-	echo
-	echo "Creating list of process that could be malware set to delete on close as maldelete.txt ... 11/24"
-	vol -f $1 -v  windows.malware.processghosting > $foldername\maldelete.txt
-	echo
-	echo "Creating list of services which could indicate rootkits as rootkits.txt ... 12/24"
-	vol -f $1 -v  windows.malware.svcdiff > $foldername\rootkits.txt
-	echo
-	echo "Scanning MBR as mbrscan.txt ... 13/24 (this could take some time)"
-	vol -f $1 -v  windows.mbrscan > $foldername\mbrscan.txt
-	echo
-	echo "Creating memory map as memmap.txt ... 14/24 (this could take some time)"
-	vol -f $1 -v  windows.memmap > $foldername\memmap.txt
-	echo
-	echo "Creating process list as processlist.txt ... 15/24"
-	vol -f $1 -v  windows.pslist > $foldername\processlist.txt
-	echo
-	echo "Creating process scan as processscan.txt ... 16/24"
-	vol -f $1 -v  windows.psscan > $foldername\rocessscan.txt
-	echo
-	echo "Creating process tree as processtree.txt ... 17/24"
-	vol -f $1 -v  windows.pstree > $foldername\processtree.txt
-	echo
-	echo "Extracting process execution cache as execcahe.txt ... 18/24"
-	vol -f $1 -v  windows.registry.amcache > $foldername\execcahe.txt
-	echo
-	echo "Creating list of scheduled tasks for loaded registry hive as tasks.txt ... 19/24"
-	vol -f $1 -v  windows.registry.scheduled_tasks > $foldername\tasks.txt
-	echo
-	echo "Creating list of active sessions as sessions.txt ... 20/24"
-	vol -f $1 -v  windows.sessions > $foldername\sessions.txt
-	echo
-	echo "Extracting process strings from memory as processstrings.txt ... 21/24 (this could take some time)"
-	vol -f $1 -v  windows.strings > $foldername\processstrings.txt --strings-file=$foldername\strings.txt
-	echo
-	echo "Performing root kit service comparison as rootkitsvcs.txt ... 22/24"
-	vol -f $1 -v  windows.svcdiff > $foldername\rootkitsvcs.txt
-	echo
-	echo "Extracting running services as services.txt ... 23/24"
-	vol -f $1 -v  windows.svcscan > $foldername\services.txt
-	echo
-	echo "Extracting TruCrypt passwords in memory as trucrypt.txt ... 24/24"
-	vol -f $1 -v  windows.truecrypt > $foldername\trucrypt.txt
-	echo
-	echo "Done."    
+    echo "Creating driver list into driverlist.txt ... 3/24"
+    vol -f $1 -v  windows.driverscan > $foldername\driverlist.txt
+    echo
+echo "Creating list of environment variables as envlist.txt ... 4/24"
+vol -f $1 -v  windows.envars > $foldername\envlist.txt
+echo
+echo "Creating list of files in memory as filelist.txt .... 5/24"
+vol -f $1 -v  windows.filescan > $foldername\filelist.txt
+echo
+echo "Creating list of owner SIDs for each process as ownersidslist.txt ... 6/24"
+vol -f $1 -v  windows.getsids > $foldername\ownersidslist.txt
+echo
+echo "Creating list of open file handles into handleslist.txt ... 7/24"
+vol -f $1 -v  windows.handles > $foldername\handleslist.txt
+echo
+echo "Creating list of memory ranges with potentially injected code as malmemcode.txt ... 8/24"
+vol -f $1 -v  windows.malfind > $foldername\malmemcode.txt
+echo 
+echo "Creating list of potentially injected by rootkit drivers as rootkitsdrivers.txt ... 9/24"
+vol -f $1 -v  windows.malware.drivermodule > $foldername\rootkitsdrivers.txt
+echo
+echo "Creating list of potentially malware hollowed proccess as malhollow.txt ... 10/24"
+vol -f $1 -v  windows.malware.hollowprocesses > $foldername\malhollow.txt
+echo
+echo "Creating list of process that could be malware set to delete on close as maldelete.txt ... 11/24"
+vol -f $1 -v  windows.malware.processghosting > $foldername\maldelete.txt
+echo
+echo "Creating list of services which could indicate rootkits as rootkits.txt ... 12/24"
+vol -f $1 -v  windows.malware.svcdiff > $foldername\rootkits.txt
+echo
+echo "Scanning MBR as mbrscan.txt ... 13/24 (this could take some time)"
+vol -f $1 -v  windows.mbrscan > $foldername\mbrscan.txt
+echo
+echo "Creating memory map as memmap.txt ... 14/24 (this could take some time)"
+vol -f $1 -v  windows.memmap > $foldername\memmap.txt
+echo
+echo "Creating process list as processlist.txt ... 15/24"
+vol -f $1 -v  windows.pslist > $foldername\processlist.txt
+echo
+echo "Creating process scan as processscan.txt ... 16/24"
+vol -f $1 -v  windows.psscan > $foldername\rocessscan.txt
+echo
+echo "Creating process tree as processtree.txt ... 17/24"
+vol -f $1 -v  windows.pstree > $foldername\processtree.txt
+echo
+echo "Extracting process execution cache as execcahe.txt ... 18/24"
+vol -f $1 -v  windows.registry.amcache > $foldername\execcahe.txt
+echo
+echo "Creating list of scheduled tasks for loaded registry hive as tasks.txt ... 19/24"
+vol -f $1 -v  windows.registry.scheduled_tasks > $foldername\tasks.txt
+echo
+echo "Creating list of active sessions as sessions.txt ... 20/24"
+vol -f $1 -v  windows.sessions > $foldername\sessions.txt
+echo
+echo "Extracting process strings from memory as processstrings.txt ... 21/24 (this could take some time)"
+vol -f $1 -v  windows.strings > $foldername\processstrings.txt --strings-file=$foldername\strings.txt
+echo
+echo "Performing root kit service comparison as rootkitsvcs.txt ... 22/24"
+vol -f $1 -v  windows.svcdiff > $foldername\rootkitsvcs.txt
+echo
+echo "Extracting running services as services.txt ... 23/24"
+vol -f $1 -v  windows.svcscan > $foldername\services.txt
+echo
+echo "Extracting TruCrypt passwords in memory as trucrypt.txt ... 24/24"
+vol -f $1 -v  windows.truecrypt > $foldername\trucrypt.txt
+echo
+echo "Done."    
   else
     echo "- file not found!"
   fi
