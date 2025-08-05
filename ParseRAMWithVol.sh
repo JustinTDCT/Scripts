72f837ddda2006da07010159903488c15722401a
JustinTDCT <166542324+JustinTDCT@users.noreply.github.com>
2025-08-05 10:23:59 -0400

    Update ParseRAMWithVol.sh

--- ParseRAMWithVol.sh
+++ ParseRAMWithVol.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+if [ $# -eq 0 ]; then
+  echo "No arguments provided."
+elif [ $# -gt 1 ]; then
+  echo "Too many arguments provided."
+else
+  clear
+  echo "Checking if file $1 exists ..."
+  if [ -f $1 ]; then
+    echo "- file found."
+  
